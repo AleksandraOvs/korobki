6f12d5b3aa738a5ebd07d6202f7ca210e2986b86
aleksandra <aleksandra.ovsia@gmail.com>
2026-05-21 20:11:28 +0300

    eds

--- wp-content/themes/gutpurple/header.php
+++ wp-content/themes/gutpurple/header.php
@@ -20,6 +20,32 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
+
+	<!-- Marquiz script start -->
+	<script>
+		(function(w, d, s, o) {
+			var j = d.createElement(s);
+			j.async = true;
+			j.src = '//script.marquiz.ru/v2.js';
+			j.onload = function() {
+				if (document.readyState !== 'loading') Marquiz.init(o);
+				else document.addEventListener("DOMContentLoaded", function() {
+					Marquiz.init(o);
+				});
+			};
+			d.head.insertBefore(j, d.head.firstElementChild);
+		})(window, document, 'script', {
+			host: '//quiz.marquiz.ru',
+			region: 'ru',
+			id: '68d95b23f9921f00197af679',
+			autoOpen: false,
+			autoOpenFreq: 'once',
+			openOnExit: false,
+			disableOnMobile: false
+		});
+	</script>
+	<!-- Marquiz script end -->
+
 </head>
 
 <body <?php body_class(); ?>>
